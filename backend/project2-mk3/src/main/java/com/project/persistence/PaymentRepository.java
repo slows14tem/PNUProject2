95c1b4aef496d04bf13eb5906855279df03b380b
slows14tem <slows14@naver.com>
2023-03-10 10:27:22 +0900

    Feat:이전 버전 제거

--- backend/project2-mk3/src/main/java/com/project/persistence/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.domain.Payment;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-
-}
